729b9cc6d842f6297bc744af681e6404cda910cc
Keandre <nhyiramensa17@gmail.com>
2025-11-06 22:04:48 +0000

    Week02:Completed Day9 - List Analyzer and value reporting system

--- week02_weekly.py
+++ week02_weekly.py
@@ -15,5 +15,53 @@ def day8():
 
     
    print(initials_remover("Jacob Price"))
+def day9():
+ def list_value_reporter(scores):
+   number_of_values = 0
+   sum_of_scores = 0  
+   if not scores:
+      print("There are no values in the list")
+      return
+   else:
+      for i in scores:
+        if isinstance(i,int) or isinstance(i,float):
+         highest_score = i
+         lowest_score = i
+         break
+      scores_above = 0   
+      for score in scores:   
+        if isinstance(score,int) or isinstance(score,float):
+           number_of_values += 1
+           sum_of_scores += score
+           
+           if highest_score < score:
+             highest_score = score
+           if lowest_score > score:
+             lowest_score = score 
+           if score > 10:
+             scores_above += 1            
+   if number_of_values == 0:
+         print("No numerical Values")
+         return
+   average = (sum_of_scores/number_of_values)
+   rounded_average = round(average , 2)
+   print(f"The average is {rounded_average}\nThe highest score is {highest_score}\nThe Lowest score is {lowest_score}\nThere are {scores_above} scores above 10  ")       
+   
+ list_value_reporter(["yo","hi"])
 
-day8()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+day9()
